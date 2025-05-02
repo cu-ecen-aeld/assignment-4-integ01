cce896d758b17026f661431b35d0fa916eb62cb4
integ <integmind@gmail.com>
2025-05-02 21:21:57 +0300

    assignment 4-2 - first

---
+++ base_external/external.mk
@@ -0,0 +1,8 @@
+########################################################
+#
+# project_base
+#
+#######################################################
+#include $(sort $(wildcard $(BR2_EXTERNAL_project_base_PATH)/package/*/*.mk))
+#include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/*.mk
+include $(BR2_EXTERNAL_project_base_PATH)/package/aesd-assignments/aesd-assignments.mk
